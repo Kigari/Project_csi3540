33e291d784a1100b960364413ea4bb91a8fc0806
Kigari <dnayi068@uottawa.ca>
2017-01-30 10:33:22 -0500

    added html and css

--- Documents/Winter2017/CSI3540/Project_csi3540/Project_csi3540_syllabus/app/controllers/pages_controller.rb
+++ Documents/Winter2017/CSI3540/Project_csi3540/Project_csi3540_syllabus/app/controllers/pages_controller.rb
@@ -1,10 +1,17 @@
 class PagesController < ApplicationController
-  def about
+
+  def login
+@title = 'Login Page';
   end
 
   def home
   end
 
   def contact
+  end
+
+  def forgotPassword
+    @title = 'Forgot Password Page';
+
   end
 end
--- Documents/Winter2017/CSI3540/Project_csi3540/Project_csi3540_syllabus/config/routes.rb
+++ Documents/Winter2017/CSI3540/Project_csi3540/Project_csi3540_syllabus/config/routes.rb
@@ -3,11 +3,13 @@ Rails.application.routes.draw do
 #set the index page / root url
   root 'pages#home'
 
-  get 'pages/about'
+  get  'pages/login'
 
   get 'pages/home'
 
   get 'pages/contact'
+
+  get  'pages/forgotPassword'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
